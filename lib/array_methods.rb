1deeb79b5dd394583f6790cdaab41add4aebd42b
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-07 20:12:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -29,5 +29,11 @@ end
 end
 
 def find_min_value(array)
-  
+  x = array[0] #hypothetical chance that the first number would be the highest valued 
+  array.length.times do |index| #going through eveyr piece of the array compared to x 
+  if array[index] < x  #and if its greater then thatll be the new x 
+  x = array[index]
+ end 
+end
+  x 
 end
