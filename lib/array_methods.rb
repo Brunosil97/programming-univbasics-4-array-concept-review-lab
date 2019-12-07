fa136a8ee82f6eca6e81d6b43a4c75d6b687d8f7
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-07 19:53:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,7 +11,7 @@
 
 def find_element_index(array, value_to_find)
 array.length.times do |count|
-  if array[count] == value_to_find
+  if array[count] == value_to_find 
     return count
   end
   nil #nil returns if it doesnt match 
