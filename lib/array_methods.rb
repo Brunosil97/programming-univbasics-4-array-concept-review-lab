dacf9c7bd4892989032d2b4e42ddeec893f6847e
Bruno Silva <brunosilvinosilva@gmail.com>
2019-12-07 19:08:17 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,11 @@
 def find_element_index(array, value_to_find)
-
+counter = 0 
+array = []
+while array[counter] do
+  puts array[counter]
+  counter += 1
+  if value_to_find == array[counter] 
+end
 end
 
 
@@ -13,16 +19,3 @@ end
 def find_min_value(array)
   # Add your solution here
 end
-
- array = []
-  value_to_find = array.length 
-  value_to_find.times do |index|
-    puts array[index]
-
-  
-   counter = 0 
-array = []
-while array[counter] do
-  puts array[counter]
-  counter += 1
-  if value_to_find == array[counter] 
